b3e2bc7887a6bcffdcdcc58d3f21cd0ee585effa
Tomasz Steć <tomeczekstecc@gmail.com>
2019-07-23 22:43:09 +0200

    sceny ukończono

--- routes/authors.js
+++ routes/authors.js
@@ -31,6 +31,7 @@ router.post('/', async (req, res) => {
   });
   try {
     const newAuthor = await author.save();
+    //res.redirect(`authors/${newAuthor.id}`)
     res.redirect('authors')
   } catch {
     res.render('authors/new', {
